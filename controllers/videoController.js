4d25ad3b3d74971c9d3cfe53c5ff48712e961d86
gabzoomer <gabriel.rodriguesxs@gmail.com>
2023-06-30 17:23:07 -0300

    :sparkles: feat: Add requisição para encontrar vídeo pelo id informado

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,3 +10,15 @@ exports.getVideo = async (req, res) => {
         res.status(500).json({ error: 'Ocorreu um erro ao buscar os livros' });
     }
 };
+
+// Obtém um vídeo pelo id
+exports.getVideoById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const data = await Video.findById(id);
+        res.status(200).json(data);
+    } catch (err) {
+        console.log('[ERRO NA BUSCA]: ' + err);
+        res.status(500).send('Não encontrado');
+    }
+};
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -4,5 +4,7 @@ const videoController = require('../controllers/videoController');
 
 // Rota para listar todos os vídeos
 router.get('/', videoController.getVideo);
+// Rota para obter um vídeo por id
+router.get('/:id', videoController.getVideoById);
 
 module.exports = router;
