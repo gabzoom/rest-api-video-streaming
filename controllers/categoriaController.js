0b6f6df91213b49e1b254fd698cd4857c9f6e961
gabzoomer <gabriel.rodriguesxs@gmail.com>
2023-07-03 10:33:14 -0300

    :sparkles: feat: Add requisição para exibir categoria pelo id

--- controllers/categoriaController.js
+++ controllers/categoriaController.js
@@ -9,3 +9,14 @@ exports.getCategoria = async (req, res) => {
         res.status(500).send('Falha ao buscar categorias');
     }
 };
+
+exports.getCategoriaById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const categoria = await Categoria.findById(id);
+        res.status(500).json(categoria);
+    } catch (err) {
+        console.log('[ERRO]: ', err);
+        res.status(500).send('Falha ao buscar a categoria');
+    }
+};
